908466c04729e22784495791b723a0da2a8294ab
nordluma <maxnordlund.95@hotmail.com>
2023-11-27 13:57:56 +0200

    feat: number guessing game

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+type Game struct {
+	targetValue     int
+	numberOfGuesses int
+	rightGuess      bool
+}
+
+func newGame(maxVal int) (*Game, error) {
+	if maxVal < 0 {
+		return nil, errors.New("Max value can't be negative")
+	}
+
+	return &Game{
+		targetValue:     rand.Intn(maxVal),
+		numberOfGuesses: 3,
+		rightGuess:      false,
+	}, nil
+}
+
+func (g *Game) Start() {
+	var guessingVal int
+	currentGuess := 0
+
+	for ; currentGuess < g.numberOfGuesses; currentGuess++ {
+		fmt.Println("Guess the number: ")
+		fmt.Scanln(&guessingVal)
+
+		if guessingVal < g.targetValue {
+			fmt.Println("The Number is larger")
+		} else if guessingVal > g.targetValue {
+			fmt.Println("The number is smaller")
+		} else {
+			g.rightGuess = true
+			currentGuess++
+			break
+		}
+	}
+
+	if g.rightGuess {
+		fmt.Printf("You won\nAmount of Guesses: %d\n", currentGuess)
+	} else {
+		fmt.Printf("Game Over\nThe right number was: %d\n", g.targetValue)
+	}
+}
+
+func main() {
+	game, err := newGame(20)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	game.Start()
+}
